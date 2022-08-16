6523a4851c0b043958437eff1718b9bfb9fa0af0
kittuvasa <kittu.black.space@gmail.com>
2022-08-16 21:43:19 +0530

    Automate PDF and Email

--- Automatically Generate a PDF and send it by Email/cars.py
+++ Automatically Generate a PDF and send it by Email/cars.py
@@ -9,7 +9,7 @@ def load_data(filename):
     """Loads the contents of filename as a JSON file."""
     with open(filename) as json_file:
         new_data = json.load(json_file)
-        data = sorted(new_data, key=lambda i: i['total_sales'])
+        data = sorted(new_data, key=lambda i: i['total_sales'], reverse=True)
     return data
 
 
